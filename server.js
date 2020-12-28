692d6c765451a71451bccf775519ee32b54ee5ac
Caleb Chaney <calebc347@gmail.com>
2020-12-27 18:50:11 -0600

    Updated server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const path = require("path");
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 // Start the API server
 app.listen(PORT, function () {
